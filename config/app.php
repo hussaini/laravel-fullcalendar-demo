661ee391aa3e7b4fd6750da8aa4be1076dac27b4
Hussaini Zulkifli <hussaini.zulkifli@gmail.com>
2020-10-07 18:23:34 +0800

    Remove unused package

--- config/app.php
+++ config/app.php
@@ -144,7 +144,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -194,8 +193,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form'      => Collective\Html\FormFacade::class,
-        'Html'      => Collective\Html\HtmlFacade::class,        
     ],
 
 ];
